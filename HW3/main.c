ab84341e6737e00e130ed1dfbe951bb94a242246
Mitchell Johnson <mitchjohnson000@gmail.com>
2017-01-30 20:48:33 -0600

    added save

--- HW3/main.c
+++ HW3/main.c
@@ -4,7 +4,10 @@
 #include <string.h>
 
 char board[105][160];
+unsigned char array_hardness[16800];
 int checkIfValid(int x,int y);
+int switch_one_global = 1;
+int switch_two_global = 1;
 void printBoard();
 void connectRooms();
 void loadDungeon();
@@ -25,29 +28,43 @@ int main(int argc, char *argv[]){
 		char * switch_one = argv[1];
 		char * switch_two = argv[2];
 		if(strcmp(switch_one,load)==0||strcmp(switch_two,load)==0){
-			loadDungeon();
+			switch_one_global = 0;
 		}
 		if(strcmp(switch_one,save)==0||strcmp(switch_two,save)==0){
-			saveDungeon();
+			switch_two_global = 0;
 			}
 	}
 
 	if(argc==2){
 		char * switch_one = argv[1];
 		if(strcmp(switch_one,load)==0){
-			loadDungeon();
+			switch_one_global = 0;
 		}
 		if(strcmp(switch_one,save)==0){
-			saveDungeon();
+			switch_two_global = 0;
 		}
+	}
+
+	if(switch_one_global == 0){
+		loadDungeon();
 	}
 
 	srand(time(NULL));
 
-	int i,j,z;
-	for(i =0;i<160;i++){
-		for(j=0;j<105;j++){
-			board[j][i] = ' ';
+	int i,j,z,hard_counter;
+	hard_counter = 0;
+	for(i =0;i<105;i++){
+		for(j=0;j<160;j++){
+			board[i][j] = ' ';
+			int hardness = rand() % 254 + 1;
+			if(i==0||j==0||i==104||j==159){
+				//printf("%s\n","border");
+				array_hardness[hard_counter] = (unsigned char)255;	
+			}else{
+				array_hardness[hard_counter] = (unsigned char) hardness;
+			}
+			hard_counter++;
+
 		}
 	}
 
@@ -69,7 +86,12 @@ int main(int argc, char *argv[]){
 			}
 		}		
 	}
+	
+	if(switch_two_global == 0){
+		saveDungeon();
+	}
 	connectRooms();
+
 	//printBoard();
 
 return 0;
@@ -134,17 +156,40 @@ void loadDungeon(){
 }
 
 void saveDungeon(){
+
 	char * home = getenv("HOME");
-	char *path = strcat(home,"/.rlg327/dungeon.txt");
+	char *path = strcat(home,"/.rlg327/dungeon.bin");
+	char * filetype = "RLG327-S2017";
+	FILE * file;
+	int version = 0;
+	unsigned int size = 16820 + (11 * 4);
+	int numOfRooms = 11;
+	char *filetype_buffer = malloc(strlen(filetype));
+	unsigned char buffer_hardness[16800];
+	unsigned int * version_buffer = malloc(sizeof(unsigned int));
+	unsigned int * size_buffer = malloc(sizeof(unsigned int));
+	int z;
+	for(z=0;z<16800;z++){
+		printf("%u\n",array_hardness[z]);
+	}
+
+
 	printf("%s\n", path);
-	FILE * file = fopen(path,"w");
+	file = fopen(path,"w+");
 	if(file==NULL){
 		printf("%s\n","File is NULL");
 	}
-	char * filetype = "RLG327-S2017";
-	//fwrite(filetype,sizeof(char),12,file);
-	int version = 0;
+	fwrite(filetype,strlen(filetype),1,file);
 	fwrite(&version,sizeof version,1,file); 
-	//fprintf(file, "%s %s %s %d", "We", "are", "in", 2012);
+	fwrite(&size,sizeof size,1,file);
+	fwrite(&array_hardness,sizeof array_hardness,1,file);
+	fseek(file, SEEK_SET, 0);
+	fread(filetype_buffer,strlen(filetype),1,file);
+	fread(version_buffer,sizeof version,1,file);
+	fread(size_buffer,sizeof size,1,file);
+	fread(buffer_hardness,sizeof array_hardness,1,file);
+
+	//printf("%s\n",buffer_hardness);
+
 	fclose(file);
 	}
