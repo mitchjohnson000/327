7cf72b03a31aeae79d69f1b5e839159ca54f86d0
Mitchell Johnson <mitchjohnson000@gmail.com>
2017-01-31 21:16:19 -0600

    load working

--- HW3/main.c
+++ HW3/main.c
@@ -9,22 +9,34 @@ unsigned char array_hardness[16800];
 int checkIfValid(int x,int y);
 int switch_one_global = 1;
 int switch_two_global = 1;
-void printBoard();
-void connectRooms();
-void loadDungeon();
-void saveDungeon();
+unsigned int version = 0;
+unsigned int size = 0;
+unsigned char * filetype;
+int load_and_save = 1;
 struct room
 {
 	int x;
 	int y;
+	int w;
+	int h;
 };
 struct room rooms[11];
 int numOfRooms = 11;
+char * home;
+char * path;
+void printBoard();
+void connectRooms();
+void loadDungeon();
+void saveDungeon(int from_load,char* filetype,int version, int size, unsigned char buffer_hardness[],struct room load_rooms[]);
+
 
 
 int main(int argc, char *argv[]){
 	char * load = "--load";
 	char * save = "--save";
+
+	home = getenv("HOME");
+	path = strcat(home,"/.rlg327/dungeon.bin");
 
 	if(argc>=3){
 		char * switch_one = argv[1];
@@ -89,8 +101,8 @@ int main(int argc, char *argv[]){
 		}		
 	}
 	
-	if(switch_two_global == 0){
-		saveDungeon();
+	if(load_and_save != 0){
+		if(switch_two_global == 0) saveDungeon(1,NULL,0,0,NULL,NULL);
 	}
 	connectRooms();
 
@@ -153,8 +165,6 @@ void connectRooms(){
 }
 
 void loadDungeon(){
-	char * home = getenv("HOME");
-	char *path = strcat(home,"/.rlg327/dungeon.bin");
 	FILE * file;
 	char *filetype_buffer = malloc(12);
 	unsigned char buffer_hardness[16800];
@@ -169,36 +179,97 @@ void loadDungeon(){
 	fread(size_buffer,4,1,file);
 	fread(buffer_hardness,16800,1,file);
 
+	version = *version_buffer;
+	size = *size_buffer;
+
+
 	printf("%s\n",filetype_buffer);
 	printf("%d\n",*version_buffer);
 	printf("%d\n",*size_buffer);
+	numOfRooms = (*size_buffer-16820)/4;
 
-	for(i=0;i<numOfRooms*4;i++){
-		uint8_t * jaldskfj = malloc(sizeof(uint8_t));
-		fread(jaldskfj,sizeof(uint8_t),1,file);
-		printf("%hhu\n",*jaldskfj);
-		free(jaldskfj);
+	struct room load_rooms[numOfRooms];
+	unsigned char load_hardness[16800];
+	printf("%d\n",numOfRooms);
+
+	for(i=0;i<numOfRooms;i++){
+		uint8_t * x = malloc(sizeof(uint8_t));
+		uint8_t * y = malloc(sizeof(uint8_t));
+		uint8_t * w = malloc(sizeof(uint8_t));
+		uint8_t * h = malloc(sizeof(uint8_t));
+
+		fread(x,sizeof(uint8_t),1,file);
+		fread(y,sizeof(uint8_t),1,file);
+		fread(w,sizeof(uint8_t),1,file);
+		fread(h,sizeof(uint8_t),1,file);
+		printf("%hhu\n",*x);
+		printf("%hhu\n",*y);
+		printf("%hhu\n",*w);
+		printf("%hhu\n",*h);
+		load_rooms[i].x = *x;
+		load_rooms[i].y = *y;
+		load_rooms[i].h = *h;
+		load_rooms[i].w = *w; 
+		free(x);
+		free(y);
+		free(h);
+		free(w);
+	}
+	fclose(file);
+	//free(path);
+
+	if(switch_two_global==0 && switch_one_global!=0){
+		saveDungeon(0,filetype_buffer,*version_buffer,*size_buffer,buffer_hardness,load_rooms);
+		load_and_save = 0;
 	}
 
 }
 
-void saveDungeon(){
+void saveDungeon(int from_load,char* filetype,int version,int size,unsigned char buffer_hardness[],struct room load_rooms[]){
 
-	char * home = getenv("HOME");
-	char *path = strcat(home,"/.rlg327/dungeon.bin");
-	char * filetype = "RLG327-S2017";
-	FILE * file;
-	int version = 0;
-	unsigned int size = 16820 + (11 * 4);
-	int z,i;
+	if(from_load == 0){
+		printf("%s\n","from_load");
+		FILE * file;
+		int version = 0;
+		unsigned int size = 16820 + (11 * 4);
+		int z,i;	
 
-	printf("%s\n", path);
-	file = fopen(path,"w+");
+	file = fopen(path,"w");
 	if(file==NULL) printf("%s\n","File is NULL");
-	fwrite(filetype,strlen(filetype),1,file);
-	fwrite(&version,sizeof version,1,file); 
-	fwrite(&size,sizeof size,1,file);
-	fwrite(&array_hardness,sizeof array_hardness,1,file);
+		fwrite(filetype,strlen(filetype),1,file);
+		fwrite(&version,sizeof version,1,file); 
+		fwrite(&size,sizeof size,1,file);
+		fwrite(&array_hardness,sizeof array_hardness,1,file);
+
+	for(i=0;i<numOfRooms;i++){
+		uint8_t x = load_rooms[i].x;
+		uint8_t y = load_rooms[i].y;
+		uint8_t w = load_rooms[i].w;
+		uint8_t h = load_rooms[i].h;
+		fwrite(&x,sizeof(uint8_t),1,file);
+		fwrite(&y,sizeof(uint8_t),1,file);
+		fwrite(&w,sizeof(uint8_t),1,file);
+		fwrite(&h,sizeof(uint8_t),1,file);
+	}	
+	fclose(file);
+
+	}else{
+
+		char * filetype_gen = "RLG327-S2017";
+		FILE * file;
+		int version_gen = 0;
+		unsigned int size_gen = 16820 + (11 * 4);
+		int z,i;
+
+		printf("%s\n", "not from load");
+		printf("%s\n", path);
+
+		file = fopen(path,"w");
+		if(file==NULL) printf("%s\n","File is NULL");
+		fwrite(filetype_gen,strlen(filetype_gen),1,file);
+		fwrite(&version_gen,sizeof version_gen,1,file); 
+		fwrite(&size_gen,sizeof size_gen,1,file);
+		fwrite(&array_hardness,sizeof array_hardness,1,file);
 
 	for(i=0;i<numOfRooms;i++){
 		uint8_t x = rooms[i].x;
@@ -210,5 +281,6 @@ void saveDungeon(){
 		fwrite(&w,sizeof(uint8_t),1,file);
 		fwrite(&h,sizeof(uint8_t),1,file);
 	}	
-	fclose(file);
+		fclose(file);		
+	}
 }
