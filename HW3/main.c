46a7ac7462dc22c350787d8f1fe11af35031b44b
Mitchell Johnson <mitchjohnson000@gmail.com>
2017-02-01 19:14:24 -0600

    everything now works

--- HW3/main.c
+++ HW3/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 #include <stdint.h>
+#include <endian.h>
 
 char board[105][160];
 unsigned char array_hardness[16800];
@@ -21,13 +22,12 @@ struct room
 	int h;
 };
 struct room rooms[11];
-int numOfRooms = 11;
 char * home;
 char * path;
 void printBoard();
-void connectRooms();
+void connectRooms(int numrooms,struct room rooms[]);
 void loadDungeon();
-void saveDungeon(int from_load,char* filetype,int version, int size, unsigned char buffer_hardness[],struct room load_rooms[]);
+void saveDungeon(int from_load,char* filetype,unsigned int version, unsigned int size, unsigned char buffer_hardness[],struct room load_rooms[],int numrooms);
 
 
 
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]){
 	char * save = "--save";
 
 	home = getenv("HOME");
-	path = strcat(home,"/.rlg327/dungeon.bin");
+	path = strcat(home,"/.rlg327/dungeon.rlg327");
 
 	if(argc>=3){
 		char * switch_one = argv[1];
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]){
 	}
 
 	srand(time(NULL));
-
+if(switch_one_global !=0){
 	int i,j,z,hard_counter;
 	hard_counter = 0;
 	for(i =0;i<105;i++){
@@ -100,13 +100,20 @@ int main(int argc, char *argv[]){
 			}
 		}		
 	}
+}
+	if(switch_one_global!=0 && switch_two_global!=0){
+		connectRooms(11,rooms);
+		printBoard();
+	}
 	
 	if(load_and_save != 0){
-		if(switch_two_global == 0) saveDungeon(1,NULL,0,0,NULL,NULL);
-	}
-	connectRooms();
+		if(switch_two_global == 0) {
+			connectRooms(11,rooms);
+			saveDungeon(1,NULL,0,0,NULL,NULL,11);
+			printBoard();
 
-	//printBoard();
+		}
+	}
 
 return 0;
 }
@@ -131,9 +138,9 @@ int checkIfValid(int x, int y){
 	return 0;
 }
 
-void connectRooms(){
+void connectRooms(int numrooms,struct room rooms[]){
 	int i,j,z;
-	for(i=0;i<10;i++){
+	for(i=0;i<numrooms -1;i++){
 		int x1 = rooms[i].x;
 		int y1 = rooms[i].y;
 
@@ -168,29 +175,32 @@ void loadDungeon(){
 	FILE * file;
 	char *filetype_buffer = malloc(12);
 	unsigned char buffer_hardness[16800];
-	unsigned int * version_buffer = malloc(sizeof(unsigned int));
-	unsigned int * size_buffer = malloc(sizeof(unsigned int));
-	int i;
+	unsigned int version_buffer;
+	unsigned int size_buffer;
+	int i,j,a,b;
 
 	file = fopen(path,"r");
 	if(file==NULL) printf("%s\n","File is NULL");
 	fread(filetype_buffer,12,1,file);
-	fread(version_buffer,4,1,file);
-	fread(size_buffer,4,1,file);
+	fread(&version_buffer,4,1,file);
+	fread(&size_buffer,4,1,file);
 	fread(buffer_hardness,16800,1,file);
 
-	version = *version_buffer;
-	size = *size_buffer;
+	version_buffer = be32toh(version_buffer);
+	size_buffer = be32toh(size_buffer);
 
-
-	printf("%s\n",filetype_buffer);
-	printf("%d\n",*version_buffer);
-	printf("%d\n",*size_buffer);
-	numOfRooms = (*size_buffer-16820)/4;
+	int numOfRooms = (size_buffer-16820)/4;
 
 	struct room load_rooms[numOfRooms];
-	unsigned char load_hardness[16800];
-	printf("%d\n",numOfRooms);
+	for(i=0;i<16800;i++){
+		array_hardness[i] = buffer_hardness[i];
+	}
+
+	for(i =0;i<105;i++){
+		for(j=0;j<160;j++){
+			board[i][j] = ' ';
+		}
+	}
 
 	for(i=0;i<numOfRooms;i++){
 		uint8_t * x = malloc(sizeof(uint8_t));
@@ -202,10 +212,12 @@ void loadDungeon(){
 		fread(y,sizeof(uint8_t),1,file);
 		fread(w,sizeof(uint8_t),1,file);
 		fread(h,sizeof(uint8_t),1,file);
-		printf("%hhu\n",*x);
-		printf("%hhu\n",*y);
-		printf("%hhu\n",*w);
-		printf("%hhu\n",*h);
+
+		for(a=0;a<*w;a++){
+			for(b=0;b<*h;b++){
+				board[*y + b][*x + a] = '.';
+			}
+		}
 		load_rooms[i].x = *x;
 		load_rooms[i].y = *y;
 		load_rooms[i].h = *h;
@@ -215,33 +227,34 @@ void loadDungeon(){
 		free(h);
 		free(w);
 	}
-	fclose(file);
-	//free(path);
 
-	if(switch_two_global==0 && switch_one_global!=0){
-		saveDungeon(0,filetype_buffer,*version_buffer,*size_buffer,buffer_hardness,load_rooms);
+	fclose(file);
+	connectRooms(numOfRooms,load_rooms);
+	printBoard();
+	if(switch_two_global==0 && switch_one_global==0){
+		saveDungeon(0,filetype_buffer,version_buffer,size_buffer,buffer_hardness,load_rooms,numOfRooms);
 		load_and_save = 0;
 	}
 
 }
 
-void saveDungeon(int from_load,char* filetype,int version,int size,unsigned char buffer_hardness[],struct room load_rooms[]){
+void saveDungeon(int from_load,char* filetype,unsigned int version,unsigned int size,unsigned char buffer_hardness[],struct room load_rooms[],int numrooms){
 
 	if(from_load == 0){
-		printf("%s\n","from_load");
 		FILE * file;
-		int version = 0;
-		unsigned int size = 16820 + (11 * 4);
 		int z,i;	
 
 	file = fopen(path,"w");
+	size = htobe32(size);
+	version = htobe32(size);
+
 	if(file==NULL) printf("%s\n","File is NULL");
 		fwrite(filetype,strlen(filetype),1,file);
 		fwrite(&version,sizeof version,1,file); 
 		fwrite(&size,sizeof size,1,file);
-		fwrite(&array_hardness,sizeof array_hardness,1,file);
+		fwrite(&buffer_hardness,sizeof buffer_hardness,1,file);
 
-	for(i=0;i<numOfRooms;i++){
+	for(i=0;i<numrooms;i++){
 		uint8_t x = load_rooms[i].x;
 		uint8_t y = load_rooms[i].y;
 		uint8_t w = load_rooms[i].w;
@@ -259,10 +272,9 @@ void saveDungeon(int from_load,char* filetype,int version,int size,unsigned char
 		FILE * file;
 		int version_gen = 0;
 		unsigned int size_gen = 16820 + (11 * 4);
+		size_gen = htobe32(size_gen);
+		version_gen = htobe32(version_gen);
 		int z,i;
-
-		printf("%s\n", "not from load");
-		printf("%s\n", path);
 
 		file = fopen(path,"w");
 		if(file==NULL) printf("%s\n","File is NULL");
@@ -271,7 +283,7 @@ void saveDungeon(int from_load,char* filetype,int version,int size,unsigned char
 		fwrite(&size_gen,sizeof size_gen,1,file);
 		fwrite(&array_hardness,sizeof array_hardness,1,file);
 
-	for(i=0;i<numOfRooms;i++){
+	for(i=0;i<numrooms;i++){
 		uint8_t x = rooms[i].x;
 		uint8_t y = rooms[i].y;
 		uint8_t w = 7;
